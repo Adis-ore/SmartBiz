78eab9498f7e4a4a94ff833ef674d80498e21548
Adigun Oreoluwa <adisoreoluwa@gmail.com>
2025-06-21 22:00:41 -0700

    Navbar update

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-full h-16 bg-[#F4F4F4]'>
+        <nav className='flex flex-col justify-center items-center'>
+            <img src="" alt="" />
+            <ul className='flex  gap-4 justify-around text-center items-center  w-[50%] p-5'>
+                <a href="">HOME</a>
+                <a href="">PROBLEM</a>
+                <a href="">SOLUTION</a>
+                <a href=""></a>
+                <a href=""></a>
+            </ul>
+        </nav>
+    </div>
   )
 }
 
